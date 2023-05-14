d0fed702e18b02b1dcf84faf9820b79d05fb8b5c
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-05-14 00:31:27 -0300

    PersonaService

--- FrontEnd/src/app/model/persona.model.ts
+++ FrontEnd/src/app/model/persona.model.ts
@@ -1,2 +1,12 @@
-export class Persona {
+export class persona {
+    id?: number;
+    nombre: string;
+    apellido: string;
+    img: String;
+
+    constructor(nombre: string, apellido: string, img: string){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.img = img;
+    }
 }
