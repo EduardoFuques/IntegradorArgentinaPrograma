c0ac4ae7c2f2ab3cc1fa9944b094124d032a99eb
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-05-15 13:55:49 -0300

    Backend Educacion

--- BackEnd/src/main/java/com/portfolio/ef/Controller/CExperiencia.java
+++ BackEnd/src/main/java/com/portfolio/ef/Controller/CExperiencia.java
@@ -32,6 +32,14 @@ public class CExperiencia {
         List<Experiencia> list = sExperiencia.list();
         return new ResponseEntity(list, HttpStatus.OK);
     }
+    
+    @GetMapping("/detail/{id}")
+    public ResponseEntity<Experiencia> getById(@PathVariable("id") int id){
+        if(!sExperiencia.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        Experiencia experiencia = sExperiencia.getOne(id).get();
+        return new ResponseEntity(experiencia, HttpStatus.OK);
+    }
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody dtoExperiencia dtoexp) {
@@ -66,7 +74,7 @@ public class CExperiencia {
         
     }
     
-    @DeleteMapping
+    @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") int id){
         if(!sExperiencia.existsById(id))
             return new ResponseEntity(new Mensaje("El id no existe"), HttpStatus.BAD_REQUEST);
