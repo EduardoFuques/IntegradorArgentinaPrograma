bd3ce5fd422bbba51bf9b43ca5d37a6d35d07628
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-05-14 21:27:47 -0300

    Funcionando API con JWT

---
+++ BackEnd/src/main/java/com/portfolio/ef/Security/Controller/Mensaje.java
@@ -0,0 +1,26 @@
+
+package com.portfolio.ef.Security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+}
---
+++ BackEnd/src/main/java/com/portfolio/ef/Security/Service/UsuarioService.java
@@ -0,0 +1,31 @@
+package com.portfolio.ef.Security.Service;
+
+import com.portfolio.ef.Security.Entity.Usuario;
+import com.portfolio.ef.Security.Repository.IUsuarioRepository;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+public class UsuarioService {
+    @Autowired
+    IUsuarioRepository iusuarioRepository;
+    
+    public Optional<Usuario> getByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.findByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existByNombreUsuario(String nombreUsuario){
+        return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
+    }
+    
+    public boolean existByEmail(String email){
+        return iusuarioRepository.existsByEmail(email);
+    }
+    
+    public void save(Usuario usuario){
+        iusuarioRepository.save(usuario);
+    }
+}
