43234843ab5d3bac67ffe5cdd88fd6e462e2aa98
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-05-15 19:15:37 -0300

    Backend HyS Skills

---
+++ BackEnd/src/main/java/com/portfolio/ef/Repository/Rhys.java
@@ -0,0 +1,12 @@
+
+package com.portfolio.ef.Repository;
+
+import com.portfolio.ef.Entity.hys;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface Rhys extends JpaRepository<hys, Integer>{
+    Optional<hys> findByNombre(String nombre);
+    public boolean existsByNombre(String nombre);
+}
