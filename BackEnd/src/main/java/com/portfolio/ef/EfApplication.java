2e6e1ca4229f46bcfee5d6ab45207fc37bf3e086
Eduardo Fuques <eduardo.fuques@unam.edu.ar>
2023-05-18 17:22:34 -0300

    configurando WebMvcConfigurer

--- BackEnd/src/main/java/com/portfolio/ef/EfApplication.java
+++ BackEnd/src/main/java/com/portfolio/ef/EfApplication.java
@@ -2,12 +2,27 @@ package com.portfolio.ef;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 @SpringBootApplication
 public class EfApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EfApplication.class, args);
+	}
+        
+        @Configuration
+	public class WebMvcConfig implements WebMvcConfigurer {
+
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**")
+	            .allowedOrigins("*")
+	            .allowedMethods("GET", "POST", "PUT", "DELETE");
+	    }
 	}
 
 }
